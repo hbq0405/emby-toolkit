82dba37d9159272eaac0997b39711987c4b60689
hbq0405 <hbq0405@hotmail.com>
2025-10-22 00:38:43 +0800

    移除冗余权限检查开关

--- emby-actor-ui/src/components/CustomCollectionsManager.vue
+++ emby-actor-ui/src/components/CustomCollectionsManager.vue
@@ -387,22 +387,6 @@
           <n-divider title-placement="left" style="margin-top: 15px;">
             附加实时筛选 (可选)
           </n-divider>
-
-          <n-form-item>
-            <template #label>
-              <n-space align="center">
-                <span>强制检查Emby原生权限</span>
-                <n-tooltip trigger="hover">
-                  <template #trigger>
-                    <n-icon :component="HelpIcon" />
-                  </template>
-                  强烈推荐！开启后，此虚拟库将自动隐藏当前用户在Emby原生媒体库中无权访问的项目。
-                </n-tooltip>
-              </n-space>
-            </template>
-            <n-switch v-model:value="currentCollection.definition.enforce_emby_permissions" />
-          </n-form-item>
-
           <n-form-item>
             <template #label>
               <n-space align="center">
@@ -1168,7 +1152,6 @@ const getInitialFormModel = () => ({
     target_library_ids: [],
     default_sort_by: 'original',
     default_sort_order: 'Ascending',
-    enforce_emby_permissions: true,
     dynamic_filter_enabled: false,
     dynamic_logic: 'AND',
     dynamic_rules: []
