bceb4593f8fe88b1852b702620d00399a832ed73
hbq0405 <hbq0405@hotmail.com>
2025-12-31 21:28:58 +0800

    继续

--- tasks/maintenance.py
+++ tasks/maintenance.py
@@ -27,7 +27,7 @@ def _prepare_data_for_insert(table_name: str, table_data: List[Dict[str, Any]]) 
         'emby_users': {'policy_json'},
         'media_metadata': {
             'emby_item_ids_json', 'subscription_sources_json', 
-            'tags_json', 'genres_json', 
+            'tags_json', 'genres_json', 'rating_json', 
             'actors_json', 'directors_json', 'studios_json', 'countries_json', 
             'keywords_json', 'last_episode_to_air_json',
             'watchlist_next_episode_json', 'watchlist_missing_info_json', 'asset_details_json',
