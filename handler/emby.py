c03e13adcc106c71056168fa2b3f811f4437a226
hbq0405 <hbq0405@hotmail.com>
2026-01-17 19:58:27 +0800

    继续

--- handler/emby.py
+++ handler/emby.py
@@ -914,7 +914,7 @@ def refresh_emby_item_metadata(item_emby_id: str,
         "api_key": emby_api_key,
         "Recursive": str(item_data.get("Type") == "Series").lower(),
         "MetadataRefreshMode": "Default",
-        "ImageRefreshMode": "Default",
+        "ImageRefreshMode": "ValidationOnly",
         "ReplaceAllMetadata": str(replace_all_metadata_param).lower(),
         "ReplaceAllImages": str(replace_all_images_param).lower()
     }
