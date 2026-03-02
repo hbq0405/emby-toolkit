93d8f8716f9b474df421aeaba4de25af20ac8110
hbq0405 <hbq0405@hotmail.com>
2026-03-02 10:37:07 +0800

    修正表名

--- emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
+++ emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
@@ -1643,7 +1643,7 @@ const tableInfo = {
   'invitations': { cn: '邀请链接', isSharable: false },
   'emby_users_extended': { cn: 'Emby用户扩展信息', isSharable: false },
   'p115_filesystem_cache': { cn: '115目录缓存', isSharable: false },
-  'p115_mediainfo_cache': {cn: '媒体信息缓存', isSharable: true }
+  'p115_mediainfo_cache': {cn: '媒体信息备份', isSharable: true }
 };
 const tableDependencies = {
   'emby_users': ['user_media_data', 'emby_users_extended'],
