b7c5e369f0a7366c7d70295e8cd713d505457c29
hbq0405 <hbq0405@hotmail.com>
2026-03-16 17:07:51 +0800

    更新版本号

--- handler/p115_service.py
+++ handler/p115_service.py
@@ -1190,7 +1190,14 @@ class SmartOrganizer:
 
         # 2.3 语言 (Languages)
         if rule.get('languages'):
-            _evaluate(self.raw_metadata.get('lang_code') in rule['languages'])
+            target_langs = list(rule['languages'])
+            # ★ 核心修复：打通 TMDB 粤语历史遗留代码 (cn) 与标准代码 (yue)
+            if 'yue' in target_langs and 'cn' not in target_langs:
+                target_langs.append('cn')
+            elif 'cn' in target_langs and 'yue' not in target_langs:
+                target_langs.append('yue')
+                
+            _evaluate(self.raw_metadata.get('lang_code') in target_langs)
 
         # 2.4 工作室 (Studios)
         if rule.get('studios'):
