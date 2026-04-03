c1b519a091b265b2b5add2949a29317a1eabb651
hbq0405 <hbq0405@hotmail.com>
2026-04-03 17:31:24 +0800

    继续

--- emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
+++ emby-actor-ui/src/components/settings/GeneralSettingsPage.vue
@@ -251,8 +251,10 @@
                           <template #prefix><n-icon :component="FolderIcon" /></template>
                         </n-input>
                         <n-button type="primary" ghost @click="openLocalFolderSelector('local_data_path', false)">选择</n-button>
-                        <template #feedback><n-text depth="3" style="font-size:0.8em;">留空使用NFO模式。</n-text></template>
                       </n-input-group>
+                      <template #feedback>
+                        <n-text depth="3" style="font-size:0.8em;">留空使用NFO模式。</n-text>
+                      </template>
                     </n-form-item>
                     <n-form-item label="TMDB API Key" path="tmdb_api_key">
                       <n-input type="password" show-password-on="mousedown" v-model:value="configModel.tmdb_api_key" placeholder="输入你的 TMDB API Key" />
